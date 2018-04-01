b3c7ff71b0f7500c276646e4820ce67a8362348f
Michael Zipp <mzipp3@mail.gatech.edu>
2018-04-01 15:13:22 -0400

    Flesh out some middleware for server

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require('express');
-const expApp = express();
+const app = express();
 const port = 3000;
 
+app.use((request,response, next) => {
+  console.log(request.headers);
+  next();
+})
+
+app.use((request, response, next) => {
+  request.chance = Math.random();
+  next();
+
+})
+
 app.get('/', (request, response) => {
-  response.send('Hello user');
+  response.send('Hello user' + request.chance);
+})
+
+app.use((err, request, response, next) => {
+  // log the error
+  console.error(err);
+  response.status(500).send('Error processing request');
 })
 
 app.listen(port, (err) => {
-  if(err){
-    return console.log('Error starting server:', err);
-  }
-
   console.log(`server listening on ${port}`);
 })
