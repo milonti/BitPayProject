ab16735df9cf5b12836fce7f266d73dc235596ce
Michael Zipp <mzipp3@mail.gatech.edu>
2018-04-02 12:26:12 -0400

    Forgot to add electron to package earlier

--- client/app.js
+++ client/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 const openpgp = require('openpgp');
 openpgp.initWorker({path: 'openpgp.worker.js'});
 openpgp.config.aead_protect = true;
@@ -8,7 +10,7 @@ const path = require('path');
 const url = require('url');
 
 function createWindow(){
-  win = new electron.BrowserWindow({width:800,height: 600})
+  win = new BrowserWindow({width:800,height: 600,show: false})
 
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
@@ -16,6 +18,17 @@ function createWindow(){
     slashes: true
   }))
 
+  win.on('ready-to-show',() => {
+    win.show();
+  })
+
+  win.on('closed',() => {
+    win=null;
+  })
 }
 
-electron.app.on('ready', createWindow)
+app.on('ready', createWindow)
+app.on('window-all-closed', () => {
+  console.log('Closing application');
+  app.quit();
+})
